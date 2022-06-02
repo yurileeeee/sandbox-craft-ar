bb76d42e5b7f0ce6c96b55d8cc164c3aabc29b0f
Yuri Lee <yurilee.tech@gmail.com>
2022-06-02 18:33:09 +0900

    Craft, Break 버튼 추가

--- Assets/Scripts/BlockPlacer.cs
+++ Assets/Scripts/BlockPlacer.cs
@@ -1,6 +1,9 @@
+//# define TEST
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BlockPlacer : MonoBehaviour
 {
@@ -10,30 +13,35 @@ public class BlockPlacer : MonoBehaviour
     [SerializeField] float maxDistance; // 최대 설치 거리
     [SerializeField] Vector3 minRange;
     [SerializeField] Vector3 maxRange;
+    [SerializeField] Button craftButton;
 
     Vector3 installPos; // 설치할 위치
     RaycastHit hit;
 
     private void Update()
     {
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.ScreenPointToRay(Input.mousePosition).direction, out hit, maxDistance, blockLayer))
+#if TEST
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.ScreenPointToRay(Input.mousePosition).direction, out hit, maxDistance, blockLayer)) // PC용
+#else
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxDistance, blockLayer)) // AR용
+#endif
         {
             Vector3 size = block.transform.lossyScale;
 
             // 설치할 위치를 나눠서 반올림해서 다시 곱하는 방식
             installPos = Vector3.Scale(Vector3Int.RoundToInt((hit.point + hit.normal.MultiplyVector(size * 0.5f)).DivideVector(size)), size);
 
-            // 클릭시 (0,0,0) ~ (15, 31, 15) 범위 안이면 블록 생성
-            if (Input.GetMouseButtonDown(0) && installPos.InRange(minRange, maxRange))
-            {
-                Instantiate(block, installPos, block.transform.rotation);
-            }
+            //// 클릭시 (0,0,0) ~ (15, 31, 15) 범위 안이면 블록 생성
+            //if (Input.GetMouseButtonDown(0))
+            //{
+            //    CraftClick();
+            //}
 
-            // 우클릭시 블록 지우기
-            if (Input.GetMouseButtonDown(1))
-            {
-                Destroy(hit.collider.gameObject);
-            }
+            //// 우클릭시 블록 지우기
+            //if (Input.GetMouseButtonDown(1))
+            //{
+            //    Destroy(hit.collider.gameObject);
+            //}
         }
 
         if (hit.transform && installPos.InRange(minRange, maxRange))
@@ -56,6 +64,18 @@ public class BlockPlacer : MonoBehaviour
         }
     }
 
+    public void CraftClick()
+    {
+        if (installPos.InRange(minRange, maxRange))
+        {
+            Instantiate(block, installPos, Quaternion.identity);
+        }
+    }
+
+    public void BreakClick()
+    {
+        Destroy(hit.collider.gameObject);
+    }
 }
 
 public static class Vector3Extensions
